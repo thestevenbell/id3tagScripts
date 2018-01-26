6794da487f15acd54909d4fd67bd37a4586faea6
Steven Bell <thestevenbell@gmail.com>
2018-01-25 21:35:53 -0500

    removed empty files and cleaned up the script so that it can be run inside any directory

--- id3tag_rename_title.sh
+++ id3tag_rename_title.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 ITER=1
-FILES="~/Downloads/Robert\ Jordan\ -\ WoT\ 07\ -\ A\ Crown\ of\ Swords/*.mp3"
-for f in ~/Downloads/Robert\ Jordan\ -\ WoT\ 07\ -\ A\ Crown\ of\ Swords/*.mp3
+for f in *.mp3
 	do
 		echo "Processing $f"
 		echo $ITER
