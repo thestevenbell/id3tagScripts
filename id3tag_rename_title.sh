d812310f4cce5130e2e1ad4738d50b541c5864fe
Steven Bell <thestevenbell@gmail.com>
2018-01-31 21:38:39 -0500

    fixed issue where 0 was not added to numbers 1 to 9 so that when in playlist the order went 1, 10, 11 and so on.

--- id3tag_rename_title.sh
+++ id3tag_rename_title.sh
@@ -4,7 +4,11 @@ ITER=1
 for f in *.mp3
 	do
 		echo "Processing $f"
-		echo $ITER
-		id3tag --song=$ITER "${f}"
+		if [ $ITER -le 9 ]
+		then
+			id3tag --song=0$ITER "${f}"
+		else
+			id3tag --song=$ITER "${f}"
+		fi
 		ITER=$(expr $ITER + 1)
 	done
